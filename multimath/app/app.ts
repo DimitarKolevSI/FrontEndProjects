e30660ac7cad8adf72eefd362eff1ebab72bf4ab
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-08 17:52:11 +0300

    Added startGame method

---
+++ multimath/app/app.ts
@@ -0,0 +1,6 @@
+function startGame(){
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting one game...'; 
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
