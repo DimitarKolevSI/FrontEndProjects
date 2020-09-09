7e43d51fe11f85f80387bce21e428aa834c1e9e5
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-09 14:13:04 +0300

    Made the app complime with Webpack

--- multimath/webpack.config.js
+++ multimath/webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },  
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
