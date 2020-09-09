2a33ce34b2166a771d008c82f3af980067171206
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-09 09:30:05 +0300

    Added tsconfig.json file

--- multimath/js/app.js
+++ multimath/js/app.js
@@ -1,5 +1,7 @@
+"use strict";
 function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting one game...';
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
