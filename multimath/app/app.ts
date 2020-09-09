af7feaefb90eaa8c8fea5ea50018dac7d3a74fec
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-09 14:30:03 +0300

    Added logPlayer method

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,8 +1,15 @@
 function startGame(){
+    const playerName: string = 'Dimitar';
+    logPlayer(playerName);
+
     var messagesElement = document.getElementById('messages');
     //By asdding ! at the end of the variable name we assert that it is not null since the compiler
     //is checking if it is null.
     messagesElement!.innerText = 'Welcome to MultiMath! Starting one game...'; 
+}
+
+function logPlayer(name){
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
