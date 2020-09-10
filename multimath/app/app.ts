8eb2610913bf47ff60c87715f23046d65cb8200c
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-10 10:39:42 +0300

    Added getInputValue and postScore functions

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -42,19 +42,35 @@
     * sendGreeting() //Good morning!
     * sendGreeting('Hello!') //Hello!
 */
+//By asdding ! at the end of the variable name we assert that it is not null since the compiler
+    //is checking if it is null.
+
 
 function startGame(){
-    let playerName: string = 'Dimitar';
+    let playerName: string | undefined= getInputValue('playername');
     logPlayer(playerName);
 
-    var messagesElement = document.getElementById('messages');
-    //By asdding ! at the end of the variable name we assert that it is not null since the compiler
-    //is checking if it is null.
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting one game...'; 
+    postScore(100,playerName)
+    
 }
 
-function logPlayer(name:string):void{
+function logPlayer(name:string = 'MultiMath Player'):void{
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID:string):string|undefined{
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    if(inputElement.value === ''){
+        return undefined;
+    }
+    else{
+        return inputElement.value;
+    }
+}
+
+function postScore(score:number, playerName:string = 'MultiMath Player'):void{
+    const scoreElement: HTMLElement|null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
