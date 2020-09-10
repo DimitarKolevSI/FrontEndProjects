6b3488f2ff76368eb33e86ca7136b9fa00b9729c
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-10 23:45:36 +0300

    Added Player class and compiled the code

--- multimath/js/app.js
+++ multimath/js/app.js
@@ -3,37 +3,37 @@
     * someValue = 42; -> OK
     * someValue="Hello world"->OK
     * someValue=true; -> Not OK
-    * 
+    *
     * Using the --strictNotNullChecks Comliper Option
     * let basicString:string;
     * basicString = null; -> X
     * basicString = undefined; -> X
-    * 
+    *
     * let nullableString: string | null;
     * nullableString = null; -> OK
     * nullableString = undefined; -> X
-    * 
+    *
     * let mysteryString: string | null | undefined;
     * let mysteryString = null; -> OK;
     * let mysteryString = undefined; -> OK;
-    * 
+    *
     * Type Assertions
     * let value:any = 5;
     * let fixedString: string = (<number>value).toFixed(4);
     * console.log(fixedString); //5.0000
-    * 
+    *
     * let fixedString: string = (value as number).toFixed(4);
     * console.log(fixedString); //5.0000
-    * 
+    *
     * Adding type annotation to functions
     * message? -> the parameter is optional. All optional parameters should be put at the end
-    * 
+    *
     * function funFunc(score:number, message?: string): string{
     *                                                                                       ^
     *                                                                                       | |
-    *                                                                                 return type 
+    *                                                                                 return type
     * }
-    * 
+    *
     * Default parameter
     * function sendGreeting(greeting:string = 'Good morning!' ):void{
     *       console.log(greeting)
@@ -42,53 +42,44 @@
     * sendGreeting('Hello!') //Hello!
 */
 //By asdding ! at the end of the variable name we assert that it is not null since the compiler
-    //is checking if it is null.
-
-
-function startGame(){
-    let playerName: string | undefined= getInputValue('playername');
+//is checking if it is null.
+function startGame() {
+    var playerName = getInputValue('playername');
     logPlayer(playerName);
-
-    postScore(100,playerName)
-    
-    postScore(-5);  
+    postScore(100, playerName);
+    postScore(-5);
 }
-
-function logPlayer(name:string = 'MultiMath Player'):void{
-    console.log(`New game starting for player: ${name}`);
+function logPlayer(name) {
+    if (name === void 0) { name = 'MultiMath Player'; }
+    console.log("New game starting for player: " + name);
 }
-
-function getInputValue(elementID:string):string|undefined{
-    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
-    if(inputElement.value === ''){
+function getInputValue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
         return undefined;
     }
-    else{
+    else {
         return inputElement.value;
     }
 }
-
-function postScore(score:number, playerName:string = 'MultiMath Player'):void{
-    let logger: (value:string) => void;
-    if (score < 0 ){
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'MultiMath Player'; }
+    var logger;
+    if (score < 0) {
         logger = logError;
     }
-    else{
+    else {
         logger = logMessage;
     }
-    const scoreElement: HTMLElement|null = document.getElementById('postedScores');
-    scoreElement!.innerText = `${score} - ${playerName}`;
-    logger(`Score: ${score}`);
-} 
-
-document.getElementById('startGame')!.addEventListener('click', startGame);
-
-const logMessage = (message:string) =>  console.log(message);
-
-function logError(err: string): void{
+    var scoreElement = document.getElementById('postedScores');
+    scoreElement.innerText = score + " - " + playerName;
+    logger("Score: " + score);
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+var logMessage = function (message) { return console.log(message); };
+function logError(err) {
     console.error(err);
 }
-
-const firstPlayer: Player = new Player();
+var firstPlayer = new Player();
 firstPlayer.name = 'Dimitar';
 console.log(firstPlayer.formatName());
---
+++ multimath/js/player.js
@@ -0,0 +1,8 @@
+var Player = /** @class */ (function () {
+    function Player() {
+    }
+    Player.prototype.formatName = function () {
+        return this.name.toUpperCase();
+    };
+    return Player;
+}());
