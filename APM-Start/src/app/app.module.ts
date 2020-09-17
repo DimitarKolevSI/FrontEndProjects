b6c8b212ad7d4bb66541c29e36c133d82ff79f68
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-17 10:00:03 +0300

    Retrieving data using HTTP

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
 import { StarComponent } from './shared/star.component'
+import { HttpClientModule } from '@angular/common/http'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { StarComponent } from './shared/star.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -11,41 +11,47 @@ import { ProductService } from './product.service'
 
 export class ProductListComponent implements OnInit{
   pageTitle:string = 'Product List!';
-    imageWidth:number = 50;
-    imageMargin:number = 2;
-    showImage:boolean = false;
-    private _listFilter: string;
-    filteredProducts: IProduct[];
-    products: IProduct[];
-    ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products; 
-    }
+  imageWidth:number = 50;
+  imageMargin:number = 2;
+  showImage:boolean = false;
+  private _listFilter: string;
+  errorMessage:string;
+  filteredProducts: IProduct[];
+  products: IProduct[];
+  ngOnInit(): void {
+      this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products;
+          this.filteredProducts = this.products; 
+        },
+        error: err => this.errorMessage = err
+      })
+  }
     
-    constructor(private productService: ProductService){
+  constructor(private productService: ProductService){
       
-    }
+  }
 
-    public get listFilter(): string {
-        return this._listFilter;
-    }
-    public set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter):this.products;
-    }
+  public get listFilter(): string {
+      return this._listFilter;
+  }
+  public set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter):this.products;
+  }
 
-    onRatingClicked(message: string): void{
-      this.pageTitle = 'Product List: ' + message;
-    }
+  onRatingClicked(message: string): void{
+    this.pageTitle = 'Product List: ' + message;
+  }
 
-    performFilter(filterBy: string):IProduct[]{
-        filterBy = filterBy.toLowerCase();
-        return this.products.filter((product: IProduct) => 
-                product.productName.toLowerCase().indexOf(filterBy)!== -1);
-    }
+  performFilter(filterBy: string):IProduct[]{
+      filterBy = filterBy.toLowerCase();
+      return this.products.filter((product: IProduct) => 
+              product.productName.toLowerCase().indexOf(filterBy)!== -1);
+  }
     
 
-    toogleImage():void{
-        this.showImage = !this.showImage;
-    }
+  toogleImage():void{
+      this.showImage = !this.showImage;
+  }
 }
