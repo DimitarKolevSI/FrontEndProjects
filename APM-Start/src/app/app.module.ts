1f99a18b7ca26cc1ca9db35e137f7a00ee661784
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-17 15:54:21 +0300

    So called working version

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,38 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ProductListComponent } from './products/product-list.component'
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
-import { StarComponent } from './shared/star.component'
-import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component'
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { AppComponent } from './app.component'
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router'
 import { WelcomeComponent } from './home/welcome.component'
-import { ProductDetailGuard } from './products/product-detail.guard'
+import { ProductModule } from './products/product.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
-    BrowserModule, 
-    FormsModule,
+    BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products',component: ProductListComponent},
-      {path: 'products/:id',
-       canActivate: [ProductDetailGuard], 
-       component: ProductDetailComponent},
       {path: 'welcome', component: WelcomeComponent},
       {path: '',redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**',redirectTo:'welcome'}
-    ])
+    ]),
+    ProductModule
+    
   ],
   bootstrap: [AppComponent]
 })
